c55f7c1b5dc8eae7504a869a88201ae11c41455c
ajf01 <ajf010@ucsd.edu>
2024-07-30 19:13:13 -0700

    Created polls page displaying questions for polls

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pollster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(__file__).parent.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
